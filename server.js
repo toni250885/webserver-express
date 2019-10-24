4e4854add541f1da96c5710b2bd82b056e976192
Toni <antonio.martinez.casfid@gmail.com>
2019-10-24 12:18:33 +0200

    primer commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+//Express HBS engine (handlebars)
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+
+
+app.get('/', (req, res) => {
+    res.render('home',{
+        nombre: 'antonio'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+})
